7b4f838bb9da23ae9e477f44d690128806d24e51
Elnaz Asadollahi-Yazdi <104828978+elnaazasd@users.noreply.github.com>
2022-05-06 17:00:36 +0200

    Add files via upload

---
+++ dataCleaning.sql
@@ -0,0 +1,63 @@
+
+/*
+Cleaning Data in SQL Queries
+*/
+
+
+Select *
+From PortfoiloProject.dbo.NashvilleHousing
+
+
+--Standardize date format
+Select SaleDate,CONVERT(Date,SaleDate)
+From PortfoiloProject.dbo.NashvilleHousing
+
+
+Update NashvilleHousing
+SET SaleDate = CONVERT(Date,SaleDate)
+
+-- If it doesn't Update properly
+
+ALTER TABLE NashvilleHousing
+Add SaleDateConverted Date;
+
+Update NashvilleHousing
+SET SaleDateConverted = CONVERT(Date,SaleDate)
+
+
+-- Populate Property Address data
+
+Select *
+From PortfoiloProject.dbo.NashvilleHousing
+--Where PropertyAddress is null
+order by ParcelID
+
+Select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress,b.PropertyAddress)
+From PortfoiloProject.dbo.NashvilleHousing a
+JOIN PortfoiloProject.dbo.NashvilleHousing b
+	on a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress is null
+
+Update a
+SET PropertyAddress = ISNULL(a.PropertyAddress,b.PropertyAddress)
+From PortfoiloProject.dbo.NashvilleHousing a
+JOIN PortfoiloProject.dbo.NashvilleHousing b
+	on a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress is null
+
+
+-- Breaking out Address into Individual Columns (Address, City, State)
+
+
+Select PropertyAddress
+From PortfoiloProject.dbo.NashvilleHousing
+--Where PropertyAddress is null
+--order by ParcelID
+
+SELECT
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 ) as Address
+, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress)) as Address
+
+From PortfoiloProject.dbo.NashvilleHousing
